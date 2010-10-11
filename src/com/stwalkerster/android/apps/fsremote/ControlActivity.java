d5c59d341b4bbcee36461d0e4ecf1d0923d65c07
Simon Walker <github@helpmebot.org.uk>
2010-10-11 00:23:34 +0000

    share project

---
+++ src/com/stwalkerster/android/apps/fsremote/ControlActivity.java
@@ -0,0 +1,19 @@
+package com.stwalkerster.android.apps.fsremote;
+
+import android.app.*;
+import android.os.*;
+import android.widget.*;
+
+public class ControlActivity extends Activity {
+	public ControlActivity() {
+		
+	}
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+		
+	}
+	
+}
---
+++ src/com/stwalkerster/android/apps/fsremote/FSRemote.java
@@ -0,0 +1,5 @@
+package com.stwalkerster.android.apps.fsremote;
+
+public class FSRemote {
+
+}
